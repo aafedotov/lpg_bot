6192d3027262993929acc9d9523930bf7048babe
aafedotov <fedotov_aa@outlook.com>
2022-09-09 09:03:29 +0300

    v0

--- lpg_bot.py
+++ lpg_bot.py
@@ -2,7 +2,7 @@
 
 import os
 
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from dotenv import load_dotenv
 
@@ -25,9 +25,37 @@ def wake_up(update, context):
     chat = update.effective_chat
     context.bot.send_message(chat_id=chat.id,
                              text='Спасибо, что включили меня')
+    button = ReplyKeyboardMarkup([['/add_lpg']], resize_keyboard=True)
+    context.bot.send_message(chat_id=chat.id,
+                             text='Спасибо, данные отправлены')
+
+
+def add_lpg(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id,
+                             text='Введите цену пропана')
+    price = None
+    while not price:
+        try:
+            price = float(update.message.text)
+        except Exception:
+            context.bot.send_message(chat_id=chat.id,
+                                     text='Некорректный ввод.')
+    context.bot.send_message(chat_id=chat.id,
+                             text='Введите количество литров')
+    value = None
+    while not price:
+        try:
+            value = float(update.message.text)
+        except Exception:
+            context.bot.send_message(chat_id=chat.id,
+                                     text='Некорректный ввод.')
+    context.bot.send_message(chat_id=chat.id,
+                             text=f'Вы ввели:{price} {value}')
 
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('add_lpg', add_lpg))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
 updater.start_polling()
 updater.idle()
